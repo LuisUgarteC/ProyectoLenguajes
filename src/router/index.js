c7fd7d1baba439cf8a60051a2fccbccfd912ebd4
JoseJavierGJ <jjosejavier@hotmail.com>
2023-06-07 21:18:26 -0600

    Nombre de las pestañas

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,17 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      title: "Pilin",
+    },
   },
   {
     path: "/recetas",
     name: "Recetas",
     component: Recetas,
+    meta: {
+      title: "Recetas",
+    },
   },
 ];
 
@@ -22,6 +28,11 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.meta.title} | 𐐘💥╾━╤デ╦︻ඞා`;
+  next(); 
 });
 
 export default router;
